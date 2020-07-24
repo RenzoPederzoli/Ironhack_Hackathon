4ac97b13f962e4e11dad5f309692b1131b617d35
lorelpayano <lorelpayano@hotmail.com>
2020-07-24 14:54:01 -0400

    started nav

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Work</li>
+                    <li>About Me</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
