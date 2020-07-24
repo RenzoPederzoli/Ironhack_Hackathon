033e7f721e8b06da27d4436dbe8c8f6ae09a5cf2
lorelpayano <lorelpayano@hotmail.com>
2020-07-24 15:46:03 -0400

    logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import "../Styles/Home.css"
+import stanley from '../images/Image 1.png'
 
 const Home = () => {
   return (
     <div className="home-wrapper">
       <h1> Hi there, <br/> I'm Stanley. </h1>
       <div className="home-img">
-        IMG
+        <img src={stanley} alt='stanley'></img>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import "../Styles/Nav.css"
+import logo from '../images/LOGO.svg'
 
 class Navbar extends Component {
     render() {
         return (
             <div>
                 <nav>
+                <div className='logo'>
+                <Link className='nav-links' to='/'>
+                    <img className='logo' src={logo} alt='logo' />
+                </Link>
+                </div>
                 <ul>
-                    <li><Link className='nav-links' to='/home'>HOME</Link></li>
+                    <li><Link className='nav-links' to='/'>HOME</Link></li>
                     <li><Link className='nav-links' to='/work'>WORK</Link></li>
                     <li><Link className='nav-links' to='/about'>ABOUT ME</Link></li>
                     <li><Link className='nav-links' to='/contact'>CONTACT</Link></li>
