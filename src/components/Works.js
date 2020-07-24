0a929164efd282e0f0d46aa1b8f3caa4b46b8437
Renzo Pederzoli <rdp73@miami.edu>
2020-07-24 16:36:03 -0400

    works

--- src/components/Works.js
+++ src/components/Works.js
@@ -3,15 +3,10 @@ import "../Styles/Works.css"
 
 const Works = () => {
   return (
-    <div className="works-wrapper">
-      <div className="work-img">
-        IMG
-      </div>
-      <div className="work-text-section">
-        <h2> Stanley </h2>
-        <span> Client </span>
-        <p> t is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-      </div>
+    <div>
+      <div className="client-pic"></div>
+      <div className="client-title"></div>
+      <div className="client-text">Julius, an adult coloring book company, has been a client of Dunder Mifflin for the past 10 years and I’ve been the sales representative for Julius for the whole duration. Since partnering with Julius, I have brought in 1 million dollars in sales of the finest paper in stock.</div>
     </div>
   );
 };
