a28b9517b1dce0931cb6e3e5dc847007c3a6e215
Renzo Pederzoli <rdp73@miami.edu>
2020-07-24 16:59:47 -0400

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import "../Styles/Nav.css"
-import logo from '../images/LOGO.svg'
 
 class Navbar extends Component {
     render() {
         return (
             <div>
                 <nav>
-                <div className='logo'>
-                <Link className='nav-links' to='/'>
-                    <img className='logo' src={logo} alt='logo' />
+                <Link to="/">
+                 <div className='logo'></div>
+
                 </Link>
-                </div>
                 <ul>
                     <li><Link className='nav-links' to='/'>HOME</Link></li>
                     <li><Link className='nav-links' to='/works'>WORK</Link></li>
